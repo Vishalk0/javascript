125f07ce553d34a42bcb1a70a0612f7495525036
Vishal Kushwaha <vishukushwaha60@gmail.com>
2025-10-12 22:26:56 -0300

    new file

---
+++ 05_iterations/03_forEach.js
@@ -0,0 +1,16 @@
+
+
+//for each loop used 
+ const coding=["vishal","vihu","Almeen","saumya","Vikash","Manish"]
+
+ //normal function
+//  coding.forEach(function(val){
+// console.log(val);
+
+//  })
+
+//Arrow function
+coding.forEach((val)=>{
+    console.log(val);
+    
+})
---
+++ 05_iterations/04_filters.js
@@ -0,0 +1,5 @@
+const userName=["vishal","Mohit","Krishna","Timu","Jagdish"]
+userName.forEach((item)=>{
+    console.log(item);
+    
+})
